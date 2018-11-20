6a57ab77de2dec63455296ade481504a03c05f93
srivr46 <sriramg.testing@gmail.com>
2018-11-20 23:29:19 +0530

    First Script uploaded to GITon Nov20 2018

---
+++ src/org/first/Arithmetic.java
@@ -0,0 +1,16 @@
+package org.first;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20,c;
+		
+		c=a+b;
+		
+		System.out.println("Addition is "+c);
+
+	}
+
+}
